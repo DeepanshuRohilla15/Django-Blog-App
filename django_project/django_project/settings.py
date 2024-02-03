efbdef7a5a536dfc2a45790c4cc4dd286b208474
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-03 23:57:01 +0530

    Testing of login

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -125,6 +125,7 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
+LOGOUT_REDIRECT_URL = 'blog-home'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
