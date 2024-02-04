39fc6f87e463aa62ac287b4ad1aab1eca8288ee1
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-05 00:32:20 +0530

    Importing the image

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,3 +9,6 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
