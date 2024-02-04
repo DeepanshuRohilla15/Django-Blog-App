41bf11011a9554f9d5df7a9d45d5b5e1203204d7
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-05 00:37:44 +0530

    Saving the image on same path so that it can override the inital image

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -18,3 +18,4 @@ class Profile(models.Model):
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
+            img.save(self.image.path)
