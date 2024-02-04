c551dfe06ab420eadc974571b5506c222196f8a1
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-05 00:36:18 +0530

    Resizing the image

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -14,3 +14,7 @@ class Profile(models.Model):
         super().save()
 
         img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
