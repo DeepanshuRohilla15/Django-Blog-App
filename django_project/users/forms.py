407ef7691ef469df85062d5dff2db2c475d3777f
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-05 00:20:43 +0530

    Validity Check

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -21,4 +21,4 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        field = ['image']
+        fields = ['image']
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -18,9 +18,12 @@ def register(request):
 
 @login_required
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
-
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
     context = {
         'u_form': u_form,
         'p_form': p_form
