14a62048e72c6f92a268f7cfa08b617811ad330a
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-05 11:00:23 +0530

    Craeting PostDetailView Route

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about')
 ]
