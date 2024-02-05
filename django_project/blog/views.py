4b7892011c266e441f3920eb5a9a93a2d8947447
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-06 00:30:01 +0530

    Adding user post url and link to user_post, home & post_detail files

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -26,9 +26,9 @@ class UserPostListView(ListView):
     context_object_name = 'posts' 
     paginate_by = 5
 
-    def get_query_set(self):
+    def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).ordery_by('-date_posted')
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
