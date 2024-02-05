253f824cc13788711396286e440e83ca20aa7452
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-05 14:51:47 +0530

    Setting the author

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -22,5 +22,9 @@ class PostCreateView(CreateView):
     model = Post
     fields = ['title', 'content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
