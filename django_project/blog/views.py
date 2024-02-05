a0993c5b5bcab8d0095f998ff42b5def5b54a375
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-05 12:13:10 +0530

    Adding Post_Create_View

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -17,7 +17,10 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
-    
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
