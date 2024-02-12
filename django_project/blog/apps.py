a186b6446a2ac871470ef2469b12febefab834b1
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-12 18:04:32 +0530

    Adding docs

--- django_project/blog/apps.py
+++ django_project/blog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+# this file is interact with database
 class BlogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'blog'
