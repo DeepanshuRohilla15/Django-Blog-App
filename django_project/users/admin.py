ced2f1a4d7bdc6247cc93dbf839c14943bb555a2
Deepanshu Rohilla <20001001027deepanshu@dcrustm.org>
2024-02-15 21:37:28 +0530

    Admin file changes

--- django_project/users/admin.py
+++ django_project/users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Profile
 
+#The admin.py file is used to display your models in the Django admin panel.
+# You can also customize your admin panel.
 admin.site.register(Profile)
